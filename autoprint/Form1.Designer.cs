475fcd67e1894f2551a2a422c974dfd768be28d7
ygkrose <ygkrose@gmail.com>
2016-01-28 16:24:03 +0800

    first upload

---
+++ autoprint/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace autoprint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(350, 40);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 40);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ autoprint/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace autoprint
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, List<string>> contents = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            printDocument1.DefaultPageSettings.Landscape = false;
+        }
+
+        private void ReadDocument()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            contents = new Dictionary<string, List<string>>();
+            DirectoryInfo di = new DirectoryInfo(fbd.SelectedPath);
+            foreach (DirectoryInfo folder in di.GetDirectories())
+            {
+                List<string> tmp = new List<string>();
+                foreach (FileInfo f in folder.GetFiles("*.jpg"))
+                {
+                    tmp.Add(f.FullName); 
+                }
+                if (tmp.Count>0)
+                    contents.Add(folder.FullName, tmp);
+            }
+            
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+            if (contents.Count > 0)
+            {
+                Rectangle m = e.MarginBounds;
+                try
+                {
+                    bool restart = false;
+                    foreach (string path in contents.Keys)
+                    {
+                        for (int i = 1; i <= contents[path].Count; i++)
+                        {
+                            if (i > 4 && i % 4 == 1)
+                            {
+                                e.HasMorePages = true;
+                                contents[path].RemoveRange(0, 4);
+                                restart = true;
+                                break;
+                            }
+                            System.Drawing.Image img = System.Drawing.Image.FromFile(contents[path][i - 1]);
+                            switch (i % 4)
+                            {
+                                case 1:
+                                    e.Graphics.DrawImage(img, 50, 50, m.Width / 2, m.Height / 2);
+                                    break;
+                                case 2:
+                                    e.Graphics.DrawImage(img, m.Width / 2 + 80, 50, m.Width / 2, m.Height / 2);
+                                    break;
+                                case 3:
+                                    e.Graphics.DrawImage(img, 50, m.Height / 2 + 100, m.Width / 2, m.Height / 2);
+                                    break;
+                                case 0:
+                                    e.Graphics.DrawImage(img, m.Width / 2 + 80, m.Height / 2 + 100, m.Width / 2, m.Height / 2);
+                                    break;
+                            }
+                        }
+                        if (contents[path].Count == 0)
+                            contents.Remove(path);
+                        if (!restart)
+                           e.HasMorePages = true;
+                    }
+                   
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+                finally
+                {
+
+                }
+            }
+            else
+                e.HasMorePages = false;
+
+        }
+
+        private void useswitch(int i)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadDocument();
+            
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+            
+        }
+    }
+}
